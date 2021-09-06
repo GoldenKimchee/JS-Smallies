9138b9af738feaf3e0ca2cc8c18745a04f3faa98
Rachel Kim <rachelkim8084@gmail.com>
2021-09-05 17:29:45 -0700

    fully functional chrome extension

--- extension/index.js
+++ extension/index.js
@@ -1,26 +1,50 @@
-let myLeads = [];
+// If a variable is not reassigned a new value, use const
+let myWebsites = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
+const prevWebsites = JSON.parse(localStorage.getItem("myWebsites"));
+
+if (prevWebsites) {
+    myWebsites = prevWebsites;
+    renderWebsites(myWebsites);
+}
+
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs){
+        console.log(tabs[0].url);
+        myWebsites.push(tabs[0].url);
+        localStorage.setItem("myWebsites", JSON.stringify(myWebsites));
+        renderWebsites(myWebsites);
+    })
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myWebsites = [];
+    renderWebsites(myWebsites);
+})
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
+    myWebsites.push(inputEl.value);
     inputEl.value = null;
-    renderLeads();
+    localStorage.setItem("myWebsites", JSON.stringify(myWebsites));
+    renderWebsites(myWebsites);
 }) //Listen to click from user, then execute func
 
-function renderLeads() {
+function renderWebsites(websites) {
     
     let listItems = "";
-
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < websites.length; i++) {
         //listItems += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
 
         // template strings (like f string)... cleaner
         //write like a string... but can add JavaScript using ${} 
         listItems += `
             <li>
-                <a href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+                <a href="${websites[i]}" target="_blank">${websites[i]}</a>
             </li>
         `
 
@@ -35,3 +59,15 @@ function renderLeads() {
     ulEl.innerHTML = listItems; //manipulate DOM once - cost efficient
 
 }
+
+
+// falsy____________
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
+
+// truthy___________
+//the rest...
